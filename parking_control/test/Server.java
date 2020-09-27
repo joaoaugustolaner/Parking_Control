21e393c2001027617b98d8c0a2b54584e7eedd60
João Augusto Laner <joaoaugustolaner@gmail.com>
2020-09-27 18:25:50 -0300

    Creating menu

--- parking_control/test/Server.java
+++ parking_control/test/Server.java
@@ -6,28 +6,61 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
 
     public static void main(String[] args) throws Exception {
-        int option;
+        int option = 0;
 
         ServerSocket reception = new ServerSocket(8080);
 
-        while(true) {
+        //connection
 
-            //connection
+//            Socket connection = reception.accept();
+//            BufferedReader client = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//            DataOutputStream server = new DataOutputStream(connection.getOutputStream());
+//
+//            //falta o menu
+//
+//
+//            option = client.read();
+        Scanner scanner = new Scanner(System.in);
 
-            Socket connection = reception.accept();
-            BufferedReader client = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            DataOutputStream server = new DataOutputStream(connection.getOutputStream());
 
-            //falta o menu
+        while (option != 5) {
 
+            System.out.println("1 - Cadastrar Veículo");
+            System.out.println("2 - Remover Veículo");
+            System.out.println("3 - Listar vagas disponíveis");
+            System.out.println("4 - Listar todas as vagas");
+            System.out.println("5 - SAIR");
 
-            option = client.read();
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.println("tá entrei");
+                    break;
+                case 2:
+                    System.out.println("aham, us guri");
+                    break;
+                case 3:
+                    System.out.println("lista disponível");
+                    break;
+                case 4:
+                    System.out.println("lista tudo");
+                    break;
+                case 5:
+                    System.out.println("LET ME OUTTTT");
+                    option = 5;
+                default:
+                    break;
+            }
 
 
         }
     }
 }
+
+
